da98c9a969853b30d8975f80e3a440023ebaa15c
JuanPineda115 <pin19087@uvg.edu.gt>
2022-01-28 18:56:47 -0600

    4.1 a 4.4

--- ht1.py
+++ ht1.py
@@ -5,5 +5,11 @@ movies = pd.read_csv("./movies.csv", encoding="latin1")
 print(
   "\nLos datos tienen", len(movies), "observaciones y", len(movies.columns), "variables."
 )
-
-print(movies.describe())
+#4.1
+movies.nlargest(10, 'budget')['title']
+#4.2
+movies.nlargest(10, 'revenue')['title']
+#4.3
+movies.nlargest(1, 'voteCount')['title']
+#4.4
+movies.nsmallest(1, 'voteAvg')['title']
