a6a93e62a72eca06862b478d023193570c14c768
Requetetin <martinamadog@gmail.com>
2022-01-28 18:10:56 -0600

    Initial description

---
+++ ht1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+movies = pd.read_csv("./movies.csv", encoding="latin1")
+
+print(
+  "\nLos datos tienen", len(movies), "observaciones y", len(movies.columns), "variables."
+)
+
+print(movies.describe())
