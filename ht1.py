b1869b6c1621af13ae4a6f6597322329792cfcb0
JuanPineda115 <pin19087@uvg.edu.gt>
2022-02-04 16:39:10 -0600

    refactor

--- ht1.py
+++ ht1.py
@@ -2,14 +2,26 @@ import pandas as pd
 
 movies = pd.read_csv("./movies.csv", encoding="latin1")
 
-print(
-  "\nLos datos tienen", len(movies), "observaciones y", len(movies.columns), "variables."
-)
-#4.1
-movies.nlargest(10, 'budget')['title']
+#??
+print("\nLos datos tienen", len(movies), "observaciones y", len(movies.columns), "variables.")
+
+#------------------ Preguntas ------------------
+#4.1 mas presupuesto
+budget = movies.nlargest(10, 'budget')['title']
+print("Las 10 peliculas con mas presupuestos fueron \n", budget)
 #4.2
-movies.nlargest(10, 'revenue')['title']
+revenues = movies.nlargest(10, 'revenue')['title']
+print("Las 10 peliculas con mas ingresos fueron \n", revenues)
 #4.3
-movies.nlargest(1, 'voteCount')['title']
+mostVoted = movies.nlargest(1, 'voteCount')['title']
+print("La pelicula con mas votos fue: \n", mostVoted)
 #4.4
-movies.nsmallest(1, 'voteAvg')['title']
+worst = movies.nsmallest(1, 'voteAvg')['title']
+print("La peor pelicula segun los votos de los usuarios es:  \n", worst)
+#4.5
+movies['releaseDate'] = pd.to_datetime(movies['releaseDate'])
+yearly = movies['releaseDate'].dt.year.value_counts()
+yearly.plot.bar()
+#4.6
+genres = movies['genres'].str.split('|', n=-1)
+genres
